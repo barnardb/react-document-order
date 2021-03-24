711aaf2e050e3273df85fee0a33b333e683483ea
Ben Barnard <barnardb@gmail.com>
2021-03-24 16:53:59 +0100

    Updates

---
+++ precache-manifest.f0dc685e3e8283d9d96cc13f44b019bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f284344a940c75641f3c4164d7141119",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2f283543da258c81126",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "e26c62c01bb11de27db2",
+    "url": "./static/js/2.2403fc83.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2403fc83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f283543da258c81126",
+    "url": "./static/js/main.154945a0.chunk.js"
+  },
+  {
+    "revision": "d7d511cbe5f47e0abca0",
+    "url": "./static/js/runtime-main.e3f994ec.js"
+  }
+]);
